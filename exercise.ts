d941fd8617382860350fd866a033c7b067d1cb45
GEORGE SENIOR MENSAH <euroclydon611@gmail.com>
2024-07-19 07:20:24 +0000

    max character in a string

--- exercise.ts
+++ exercise.ts
@@ -1,4 +1,25 @@
-// Given a string, return the character that is most commonly used in the string
+//1. Given a string, w the character that is most commonly used in the string
 // --- Examples
 // maxChar("abcccccccd") = "c"
 // maxChar("apple 1231111") = "1"
+
+const maxCharEx = (str: string): string => {
+  const charMap: { [key: string]: number } = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+};
+
+console.log(maxCharEx("abcccccccd"));
